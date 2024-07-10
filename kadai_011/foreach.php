a637cb2dd252833c4cef0cfb79029804d108a1b9
gumi25-art <160398203+gumi25-art@users.noreply.github.com>
2024-07-10 09:32:37 +0800

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+<p>
+         <?php
+        $vegetable_name = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+                
+               foreach ($vegetable_name as $key => $value) {
+                echo "{$key}: {$value}<br>";
+            }
+         ?>
+     </p>
+     
+</body>
+
+</html>
+
